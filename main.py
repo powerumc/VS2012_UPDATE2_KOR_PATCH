854f8d8fb2ffa130886a4369fd9afda2b253d346
Junil, Um <powerumc@gmail.com>
2013-06-10 01:22:30 +0900

    extract zip function

--- main.py
+++ main.py
@@ -5,36 +5,43 @@ import io
 
 if __name__ == '__main__':
 
-	#print(platform.processor() == True )
+    ISO = "https://github.com/powerumc/VS2012_UPDATE2_KOR_PATCH/archive/master.zip"
+    CHUNK_SIZE = 1024      # 1MB
 
-	#reg = _winreg.ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+    r = requests.get(ISO)
+    total_size = int(r.headers['content-length'])
+    pbar = progressbar.ProgressBar(maxval=total_size).start()
 
-	#key = _winreg.OpenKey(reg, "SOFTWARE\Wow6432Node\Microsoft\VisualStudio\11.0\InstallDir")
-	#print(key);
+    f = open('a.zip', 'wb')
 
-	#files = ['http://docs.python-requests.org/en/latest/index.html', 'https://github.com/powerumc/VS2012_UPDATE2_KOR_PATCH/blob/master/BasicMvcWebApplicationProjectTemplatev4.1.csaspx.zip']
-	#CHUNK_SIZE = 1024 * 1024 # 1MB
+    file_contents = ""
+    for chunk in r.iter_content(chunk_size=CHUNK_SIZE):
+        file_contents += chunk
+        pbar.update(len(file_contents))
+        f.write(chunk)
 
-	
-	ISO = "https://github.com/powerumc/VS2012_UPDATE2_KOR_PATCH/archive/master.zip"
-	CHUNK_SIZE = 1024 # 1MB
+    f.close()
 
-	r = requests.get(ISO)
-	total_size = int(r.headers['content-length'])
-	pbar = progressbar.ProgressBar(maxval=total_size).start()
-
-	f = open('a.zip', 'wb')
-
-	file_contents = ""
-	for chunk in r.iter_content(chunk_size=CHUNK_SIZE):
-		file_contents += chunk
-		pbar.update(len(file_contents))
-		f.write(chunk)
-
-	f.close()
+    zip = zipfile.ZipFile("a.zip", "r")
+    zip.extractall(".")
 
 
-	pass;
 
+
+
+
+
+
+
+
+#print(platform.processor() == True )
+
+#reg = _winreg.ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+
+#key = _winreg.OpenKey(reg, "SOFTWARE\Wow6432Node\Microsoft\VisualStudio\11.0\InstallDir")
+#print(key);
+
+#files = ['http://docs.python-requests.org/en/latest/index.html', 'https://github.com/powerumc/VS2012_UPDATE2_KOR_PATCH/blob/master/BasicMvcWebApplicationProjectTemplatev4.1.csaspx.zip']
+#CHUNK_SIZE = 1024 * 1024 # 1MB
 
 
